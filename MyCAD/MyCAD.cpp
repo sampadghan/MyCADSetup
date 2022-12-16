3f00c3488d973dd0fe00cf790ed25900b5cbc50e
sampadghan <sameer.padghan@gsource.in>
2022-12-16 17:24:32 +0530

    Trying to create and read the database file(.dwg)

--- MyCAD/MyCAD.cpp
+++ MyCAD/MyCAD.cpp
@@ -1,17 +1,45 @@
 #include "MyCAD.h"
 #include "OdaCommon.h"
 #include "MyService.h"
+#include "DbBaseHostAppServices.h"
 #include "OdError.h"
+#include "DbCircle.h"
+#include "DbLine.h"
+#include "DbBlockTable.h"
+#include "DbBlockTableRecord.h"
+#include <QMessageBox>
 
-MyCAD::MyCAD(QWidget *parent)
-    : QMainWindow(parent)
+MyCAD::MyCAD(QWidget* parent)
+	: QMainWindow(parent)
 {
-    OdRxObjectImpl<MyService> svcs;
+	OdRxObjectImpl<MyService> svcs;
+	if (&svcs == NULL)
+	{
+		exit(1);
+	}
 	odInitialize(&svcs);
-    ui.setupUi(this);
+	try
+	{
+		OdDbDatabasePtr pDb;
+		pDb = svcs.readFile("Drawing.dwg");
+	}
+	catch (OdError_FileException)
+	{
+		QMessageBox msgBox;
+		msgBox.setText("OdError_FileException");
+		msgBox.exec();
+	}
+	catch (OdError)
+	{
+		QMessageBox msgBox;
+		msgBox.setText("OdError has occurred.");
+		msgBox.exec();
+	}
+	ui.setupUi(this);
+	//odUninitialize();
 }
 
 MyCAD::~MyCAD()
 {
-    odUninitialize();
+	odUninitialize();
 }
