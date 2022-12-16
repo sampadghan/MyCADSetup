05942d255574477289b7b52fd65ac49071546085
sampadghan <sameer.padghan@gsource.in>
2022-12-16 19:20:05 +0530

    Reading the DataBase File(.dwg)

--- MyCAD/MyCAD.cpp
+++ MyCAD/MyCAD.cpp
@@ -7,6 +7,7 @@
 #include "DbLine.h"
 #include "DbBlockTable.h"
 #include "DbBlockTableRecord.h"
+#include "DbDatabase.h"
 #include <QMessageBox>
 
 MyCAD::MyCAD(QWidget* parent)
@@ -18,9 +19,10 @@ MyCAD::MyCAD(QWidget* parent)
 		exit(1);
 	}
 	odInitialize(&svcs);
+	svcs.setRecomputeDimBlocksRequired(false);
+	OdDbDatabasePtr pDb = svcs.createDatabase(false);
 	try
 	{
-		OdDbDatabasePtr pDb;
 		pDb = svcs.readFile("Drawing.dwg");
 	}
 	catch (OdError_FileException)
@@ -29,17 +31,41 @@ MyCAD::MyCAD(QWidget* parent)
 		msgBox.setText("OdError_FileException");
 		msgBox.exec();
 	}
-	catch (OdError)
+	catch (OdError& error)
 	{
 		QMessageBox msgBox;
+		OdString myerror = error.description();
 		msgBox.setText("OdError has occurred.");
 		msgBox.exec();
 	}
+	OdDbBlockTablePtr pTable = pDb->getBlockTableId().safeOpenObject(OdDb::kForWrite);
+	OdDbObjectId blockTableID = pDb->getBlockTableId();
+
+	/**********************************************************************/
+	/* Open the Model Space block                                         */
+	/**********************************************************************/
+	OdDbBlockTableRecordPtr pModelSpace = pDb->getModelSpaceId().safeOpenObject(OdDb::kForWrite);
+
+	OdDbLinePtr pLine = OdDbLine::createObject();
+	OdDbObjectId objId = pModelSpace->appendOdDbEntity(pLine);
+	pLine->setStartPoint(OdGePoint3d(2, 0, 0));
+	pLine->setEndPoint(OdGePoint3d(4, 2, 0));
+	pLine->setColorIndex(12);
+
+	/**********************************************************************/
+	/* Saving the created database as a file                              */
+	/**********************************************************************/
+	pDb->writeFile("Drawing.dwg", OdDb::kDwg, OdDb::vAC24);
+
+	/**********************************************************************/
+	/* Delete the Database instance                                       */
+	/**********************************************************************/
+	pDb.release();
 	ui.setupUi(this);
-	//odUninitialize();
+	odUninitialize();
 }
 
 MyCAD::~MyCAD()
 {
-	odUninitialize();
+	//odUninitialize();
 }
